d32b97db623c9c5aea2940594fa7f46af2b64be7
rewadiasuraj-cell <rewadiasuraj@gmail.com>
2026-08-18 17:03:13 +0530

    Update sitemap lastmod and ping Googlebot for immediate favicon crawling

--- update_and_ping_google.js
+++ update_and_ping_google.js
@@ -1,4 +1,9 @@
-<?xml version="1.0" encoding="UTF-8"?>
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
+
+// 1. Update sitemap.xml with updated lastmod and complete URLs
+const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://www.vishitjourney.com/</loc>
@@ -43,3 +48,23 @@
     <priority>0.7</priority>
   </url>
 </urlset>
+`.trim();
+
+fs.writeFileSync('d:\\vishit-journeys\\sitemap.xml', sitemapContent, 'utf8');
+console.log('Updated sitemap.xml with 2026-08-18 lastmod timestamp.');
+
+// 2. Ping Google and Bing Webmaster Crawlers
+const pingUrls = [
+  'http://www.google.com/ping?sitemap=https://www.vishitjourney.com/sitemap.xml',
+  'http://www.google.com/ping?sitemap=https://vishitjourney.com/sitemap.xml',
+  'http://www.bing.com/ping?sitemap=https://www.vishitjourney.com/sitemap.xml'
+];
+
+pingUrls.forEach(url => {
+  const req = (url.startsWith('https') ? https : http).get(url, (res) => {
+    console.log(`Pinged crawler: ${url} => Status: ${res.statusCode}`);
+  });
+  req.on('error', (e) => {
+    console.log(`Ping error for ${url}: ${e.message}`);
+  });
+});
